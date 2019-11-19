954d53456b7df77bdfb4a15e0bbc59af42f06c32
徐梓琅 <ksbc24v@gmail.com>
2019-11-20 00:34:48 +0800

    new project

--- conbine_two_pictures/cobine_two_pictures.py
+++ conbine_two_pictures/cobine_two_pictures.py
@@ -4,8 +4,11 @@
 # @Email : ksbc24v@gmail.com
 # @Github: https://github.com/ZRSGEEKCABIN
 # @Date  : 2019-11-20
-# @Desc  : 
+# @Desc  : 此脚本用于将两张图片和并为一张
+import os
+import cv2
 
+def main()
 
 if __name__ == '__main__':
     pass
