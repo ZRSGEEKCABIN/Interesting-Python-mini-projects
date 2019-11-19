47a64f8769fb53891cbc5e0be4c8366ae040b992
徐梓琅 <ksbc24v@gmail.com>
2019-11-20 00:34:25 +0800

    add .gitignore

---
+++ conbine_two_pictures/cobine_two_pictures.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @File  : cobine_two_pictures.py
+# @Author: ZRSGEEKCABIN
+# @Email : ksbc24v@gmail.com
+# @Github: https://github.com/ZRSGEEKCABIN
+# @Date  : 2019-11-20
+# @Desc  : 
+
+
+if __name__ == '__main__':
+    pass
